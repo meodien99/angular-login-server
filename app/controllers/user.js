fcbbc2f993716b0747edbaafad762303298c974e
Le Bien Thuy <lebienthuy@gmail.com>
2015-03-09 17:01:11 +0700

    fix chart

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -6,5 +6,6 @@ var userRepo = new UserRepository();
 
 router.get('/', userRepo.getUser.bind(userRepo));
 router.get('/ccu', userRepo.getCCU.bind(userRepo));
+router.get('/feedback', userRepo.getFeedback.bind(userRepo));
 
 module.exports = router;
